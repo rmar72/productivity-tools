ce8aba87187bd4352ed2c5cc1847419e664d595f
rmar72 <rsolm72@gmail.com>
2025-01-04 14:09:37 -0700

    some all around app fixes

--- TodoApp/src/db/seed.sql
+++ TodoApp/src/db/seed.sql
@@ -4,3 +4,9 @@ CREATE TABLE IF NOT EXISTS todos (
     title VARCHAR(255) NOT NULL,
     completed BOOLEAN NOT NULL DEFAULT false
 );
+
+-- Insert initial data
+INSERT INTO todos (title, completed) VALUES 
+('Learn Docker', false),
+('Write Seed Script', false),
+('Run the App', true);
